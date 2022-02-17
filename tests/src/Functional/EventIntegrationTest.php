5461345d0b98363aadea88c9444c8b0d739ba7ed
Sergio Elvira Perez <sergioelviraperez@gmail.com>
2022-02-17 13:01:16 +0100

    OEL-1021: Image test added on Event, News description test typo.

--- tests/src/Functional/EventIntegrationTest.php
+++ tests/src/Functional/EventIntegrationTest.php
@@ -11,7 +11,7 @@ use Drupal\file\Entity\File;
 use Drupal\Tests\TestFileCreationTrait;
 
 /**
- * Ensure OE Starter Content Event are created.
+ * Ensure OE Starter Content Event is created.
  *
  * @group oe_starter_content
  */
@@ -78,6 +78,24 @@ class EventIntegrationTest extends BrowserTestBase {
     ]);
     $media_document->save();
 
+    // Create an image entity to be embedded.
+    $new_file = File::create([
+      'uri' => $this->getTestFiles('image')[0]->uri,
+    ]);
+    $new_file->save();
+    $media_image = Media::create([
+      'bundle' => 'image',
+      'name' => 'Starter Image test',
+      'oe_media_image' => [
+        [
+          'target_id' => $new_file->id(),
+          'alt' => 'Starter Image test alt',
+          'title' => 'Starter Image test title',
+        ],
+      ],
+    ]);
+    $media_image->save();
+
     // Create an event.
     $this->drupalGet('node/add/oe_event');
     $page->fillField('Title', 'Example Event title');
@@ -114,6 +132,30 @@ class EventIntegrationTest extends BrowserTestBase {
     $assert_session->responseContains('text-0.txt');
     $assert_session->pageTextContains('Sat, 01/22/2022 - 02:12');
     $assert_session->pageTextContains('Thu, 02/24/2022 - 20:00');
+
+    // Create an event with image.
+    $this->drupalGet('node/add/oe_event');
+    $page->fillField('Title', 'Example Event image title');
+    $page->fillField('Content', 'Example Event image content');
+    $page->fillField('Introduction', 'Example Event image introduction');
+    $media_name = $media_image->getName() . ' (' . $media_image->id() . ')';
+    $page->fillField('oe_documents[0][target_id]', $media_name);
+    $edit = [
+      'Country' => 'BE',
+    ];
+    $this->submitForm($edit, 'Save');
+    $page->fillField('Street address', 'Rue Philippe Le Bon 1');
+    $page->fillField('Postal code', '1000');
+    $page->fillField('City', 'Bruxelles');
+    $page->fillField('oe_event_dates[0][value][date]', '2022-01-22');
+    $page->fillField('oe_event_dates[0][value][time]', '02:12:22');
+    $page->fillField('oe_event_dates[0][end_value][date]', '2022-02-24');
+    $page->fillField('oe_event_dates[0][end_value][time]', '20:00:00');
+    $page->pressButton('Save');
+
+    $assert_session->responseContains('image-test.png');
+    $assert_session->responseContains('Starter Image test');
+    $assert_session->responseContains('Starter Image test alt');
   }
 
 }
