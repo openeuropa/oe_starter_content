f0197ccb2566aebab2c3c56f9ff64eec5708589a
escuriola <escuriola@gmail.com>
2022-06-03 15:06:13 +0200

    OEL-1651: Docblock. Fix comment in the post_update.

--- modules/oe_starter_content_news/oe_starter_content_news.post_update.php
+++ modules/oe_starter_content_news/oe_starter_content_news.post_update.php
@@ -10,7 +10,7 @@ declare(strict_types = 1);
 use Drupal\field\Entity\FieldConfig;
 
 /**
- * Enable OE Webtools Social share, Cookie consent and Page feedback components.
+ * Set default date for News publication date.
  */
 function oe_starter_content_news_post_update_00001(&$sandbox) {
   $field = FieldConfig::load('node.oe_sc_news.oe_publication_date');
